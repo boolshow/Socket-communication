3c0fe2afe550a39502b46607516727ad8732d190
fanfanqianfan <fanfanqianfan@163.com>
2023-01-05 16:34:43 +0800

    添加项目文件。

---
+++ socket-ip-analog-communication/.svn/pristine/c8/c81ee49e45806b54771ddd6225ede417ad690aa7.svn-base
@@ -0,0 +1,128 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+int _port = 12345;
+int usernumber = 0;
+Socket clientSocket = null;
+//将远程客户端的IP地址和Socket存入集合中
+Dictionary<string, Socket> dicSocket = new();
+
+Console.WriteLine("Hello, World!");
+StartListen();
+Console.ReadLine();
+
+void StartListen()
+{
+    try
+    {
+        // 实例化套接字(IP4寻找协议,流式协议,TCP协议)
+        Socket _socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+        //创建IP对象
+        List<IPAddress> iPAddresses = GetIPAddresses();
+        foreach (IPAddress ip in iPAddresses)
+        {
+            Console.WriteLine(ip.ToString());
+        }
+        //创建IP地址和端口号对象
+       // IPAddress ip = IPAddress.Any; 
+        IPEndPoint point = new(iPAddresses[0], _port);
+
+        //_socket.Bind(new IPEndPoint(iPAddresses[0], _port));
+        _socket.Bind(point);
+        //设置最大连接数
+        _socket.Listen(int.MaxValue);
+        Console.WriteLine("监听{0}消息成功", _socket.LocalEndPoint);
+        //开始监听
+        Task.Run(() => ListenClientConnect(_socket));
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
+    /// <summary>
+    /// 监听客户端连接
+    /// </summary>
+void ListenClientConnect(Socket _socket)
+{
+    try
+    {
+        while (true)
+        {
+            try
+            {
+                //Socket创建的新连接
+                clientSocket = _socket.Accept();
+            } catch (Exception ex) {
+                //提示套接字监听异常     
+                Console.WriteLine(ex.Message);
+                break;
+            }
+            //客户端网络结点号  
+            string remoteEndPoint = clientSocket.RemoteEndPoint.ToString();
+            //将远程连接的客户端的IP和Socket存入集合中
+            dicSocket.Add(remoteEndPoint, clientSocket);
+            //获取客户端的IP和端口号  
+            IPAddress clientIP = (clientSocket.RemoteEndPoint as IPEndPoint).Address;
+            int clientPort = (clientSocket.RemoteEndPoint as IPEndPoint).Port;
+            usernumber++;
+            string text = string.Format("服务端连接成功!当前客户端在线用户数量{0}-----{1}", usernumber,DateTime.Now);
+            string sendmsg = string.Format("本地IP:{0},clientIP：{1}", clientIP.ToString(), clientPort);
+            clientSocket.Send(Encoding.UTF8.GetBytes(text));
+            clientSocket.Send(Encoding.UTF8.GetBytes(sendmsg));
+            //接收客户端消息线程
+            Task.Run(() => ReceiveMessage(clientSocket));
+        }
+    }
+    catch (Exception)
+    {
+    }
+}
+
+/// <summary>
+/// 接收客户端消息
+/// </summary>
+/// <param name="socket">来自客户端的socket</param>
+void ReceiveMessage(Socket clientSocket)
+{
+    while (true)
+    {
+        try
+        {
+            byte[] vs = new byte[1024];
+            //获取从客户端发来的数据
+            int length = clientSocket.Receive(vs);
+            Console.WriteLine("接收客户端{0},消息{1}", clientSocket.RemoteEndPoint, Encoding.UTF8.GetString(vs, 0, length));
+            clientSocket.Send(vs);
+        }
+        catch (Exception ex)
+        {
+            dicSocket.Remove(clientSocket.RemoteEndPoint.ToString());
+            usernumber--;
+            Console.WriteLine(ex.Message);
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            break;
+        }
+    }
+}
+
+//获取ip地址
+List<IPAddress> GetIPAddresses()
+{
+    List<IPAddress> iPAddresses = new();
+
+    foreach (IPAddress ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+    {
+        if (ip.AddressFamily.ToString() == "InterNetwork")
+        {
+            iPAddresses.Add(ip);
+        }
+
+    }
+    return iPAddresses;
+}
---
+++ socket-ip-analog-communication/客户端/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using static System.Collections.Specialized.BitVector32;
+
+namespace 客户端
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //循环用到socket
+        public Socket socket = null;
+        //发送消息需要提供
+        public IPEndPoint sendPoint = new(IPAddress.Broadcast, 12345);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initenter();
+        }
+
+        private void Initenter()
+        {
+            try
+            {
+                List<IPAddress> iPAddresses = GetIPAddresses();
+                //创建负责通信的Socket
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp)
+                {
+                    SendTimeout = 10000
+                };
+                IPEndPoint point = new(iPAddresses[0], 12345);
+
+                //获得要链接的远程服务器应用程序的IP地址和端口号
+                socket.Connect(point);
+
+                Console.WriteLine("连接成功");
+                Task.Run(Recive);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// 不停的接受服务器发来的消息
+        /// </summary>
+        public void Recive()
+        {
+            Action<string, string> action = new(UIupdate);
+            EndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+            while (true)
+            {
+                //缓冲区
+                byte[] vs = new byte[1024];
+                //参数  缓冲区   获取到的内容保存到了iPEndPoint
+                if (!socket.Connected)
+                {
+                    MessageBox.Show("与服务器断开链接");
+                }
+                else
+                {
+                    int r = socket.Receive(vs);
+                    string p = Encoding.UTF8.GetString(vs);
+                    //UI界面的内容和获取到的内容在不同的线程中，所以要跨线程调用
+                    Dispatcher.Invoke(action, DateTime.Now.ToString(), p);//在这里执行action，获取ip和内容
+                }
+            }
+        }
+
+        //对ui线程进行操作
+        private void UIupdate(string arg1, string arg2)
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append(arg1);
+            stringBuilder.Append(':');
+            stringBuilder.Append(arg2);
+            Label label = new()
+            {
+                Content = stringBuilder.ToString()
+            };
+            MsgWindow.Children.Add(label);
+        }
+
+        //获取ip地址
+        public static List<IPAddress> GetIPAddresses()
+        {
+            List<IPAddress> iPAddresses = new();
+
+            foreach (IPAddress ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    iPAddresses.Add(ip);
+                }
+
+            }
+            return iPAddresses;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //发送消息
+
+            byte[] vs = Encoding.UTF8.GetBytes(MsgBox.Text);
+            socket.SendTo(vs, sendPoint);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //释放socket
+            socket?.Close();
+            Application.Current.Shutdown(0);
+        }
+    }
+}
